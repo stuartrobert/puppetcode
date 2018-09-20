ea0cf9d863f9b9e0012be082f2ac9d75eabbe1af
Robert Stuart <robertmstuart@gmail.com>
2018-09-20 21:16:47 +1000

    updated csr.sh to produce structed fact

--- certs_to_sign/facts.d/csr.sh
+++ certs_to_sign/facts.d/csr.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo '{'
 for fn in `find /etc/pki/tls/csr -name '*.csr' -type f` ; do
   key=`basename $fn`
   value=`cat $fn | base64 -w 0`
-  echo "$key"=\'"$value"\'
+  echo "  $key"' => {'
+  echo '    csr => "'$value'",'
+  echo '  },'
 done
+echo '}'
