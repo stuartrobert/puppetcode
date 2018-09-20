abb034f8e9609e9f3deed569138231028c2020a6
Robert Stuart <robertmstuart@gmail.com>
2018-09-20 21:39:50 +1000

    finalized csr.sh

--- certs_to_sign/facts.d/csr.sh
+++ certs_to_sign/facts.d/csr.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 echo '{'
+echo '  "x509_csrs": {'
 for fn in `find /etc/pki/tls/csr -name '*.csr' -type f` ; do
   key=`basename $fn`
   value=`cat $fn | base64 -w 0`
-  echo "  $key"' => {'
-  echo '    csr => "'$value'",'
-  echo '  },'
+  echo '    "'$key'": {'
+  echo '      "csr": "'$value'",'
+  echo '    },'
 done
+echo '  }'
 echo '}'
